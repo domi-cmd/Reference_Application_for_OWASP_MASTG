dcd29777f3419872fec330d46b4549ffc3d860e8
domi-cmd <dokro@gmx.ch>
2025-10-08 12:07:54 +0200

    Added more android activities and buttosn to expand on core app structure

--- apps/app_01/app/src/main/java/com/example/app_01/MainActivity.java
+++ apps/app_01/app/src/main/java/com/example/app_01/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.app_01;
 
-import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Define UI elements
+    private Button login_button;
+    private Button register_button;
+    private Button settings_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate UI elements
+        login_button = findViewById(R.id.login_button);
+        register_button = findViewById(R.id.register_button);
+        settings_button = findViewById(R.id.settings_button);
+
+        // Add listeners to buttons
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        settings_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ apps/app_01/app/src/main/java/com/example/app_01/RegisterActivity.java
@@ -0,0 +1,36 @@
+package com.example.app_01;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    // Define UI elements
+    private Button main_menu_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        main_menu_button = findViewById(R.id.main_menu_button);
+
+        // Add listeners to buttons
+        main_menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}

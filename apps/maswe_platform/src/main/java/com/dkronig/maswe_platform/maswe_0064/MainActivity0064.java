dca13ecb749e639813b2785c3bb8fa8ed557187f
domi-cmd <dokro@gmx.ch>
2025-11-19 14:54:13 +0100

    Replace boilerplate layouts in maswe_platform with references to templates in common

--- apps/maswe_platform/src/main/java/com/dkronig/maswe_platform/maswe_0064/MainActivity0064.java
+++ apps/maswe_platform/src/main/java/com/dkronig/maswe_platform/maswe_0064/MainActivity0064.java
@@ -10,7 +10,7 @@ public class MainActivity0064 extends BaseActivityTemplate {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_0064);
+        setContentView(R.layout.activity_main_template);
 
         // Map UI buttons, handled in BaseActivityTemplate
         bindButtons(Map.of(

e085393639c8ea0e7fb0a244ce7878d59e76bd75
domi-cmd <dokro@gmx.ch>
2025-12-15 18:56:31 +0100

    Added vulnerability to login activity of maswe_0053

--- apps/maswe_platform/src/main/java/com/dkronig/maswe_platform/maswe_0053/LoginActivity.java
+++ apps/maswe_platform/src/main/java/com/dkronig/maswe_platform/maswe_0053/LoginActivity.java
@@ -2,6 +2,9 @@ package com.dkronig.maswe_platform.maswe_0053;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
 import com.dkronig.common.BaseLoginActivity;
 import com.dkronig.maswe_platform.R;
 
@@ -12,6 +15,22 @@ public class LoginActivity extends BaseLoginActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EditText email_field = findViewById(R.id.et_email);
+        EditText password_field = findViewById(R.id.et_password);
+
+        // Remove password obfuscation, enable auto complete and auto correct
+        password_field.setInputType(InputType.TYPE_CLASS_TEXT |
+                InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE |
+                InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
+
+        // Same for email
+        email_field.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE |
+                InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
+
+        // Enable copy and paste functionality by setting it to null (default)
+        password_field.setCustomSelectionActionModeCallback(null);
+        
         try {
             encryptionHandler = new EncryptionHandler();
         } catch (Exception e) {

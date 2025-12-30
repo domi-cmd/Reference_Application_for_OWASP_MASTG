f1c5ee24b09d2bdc69f97c974c2c1a42a7e8fb42
domi-cmd <dokro@gmx.ch>
2025-12-30 12:27:50 +0100

    Refactored encryption handler of maswe_0012, added helper methods, docstrings, replaced hardcoded values with variables and sorted imports

--- apps/maswe_crypto/src/main/java/com/dkronig/maswe_crypto/maswe_0012/EncryptionHandler.java
+++ apps/maswe_crypto/src/main/java/com/dkronig/maswe_crypto/maswe_0012/EncryptionHandler.java
@@ -7,34 +7,120 @@ import android.util.Base64;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
-
 import javax.crypto.Cipher;
 
 public class EncryptionHandler {
     private static final String rsaKeyAlias = "maswe_0012_rsa_key";
+    private static final String CIPHER_TRANSFORMATION = "RSA/ECB/PKCS1Padding";
+    private static final String SIGNING_ALGORITHM = "SHA1withRSA";
+    private static final String KEYSTORE = "AndroidKeyStore";
 
+    /**
+     * Loads AndroidKeystore to manage RSA Key used for encryption and decryption.
+     * Creates and stores said key in AndroidKeystore.
+     *
+     * Features:
+     *  - Generates an RSA key
+     *  - Key is Base64-encoded for storage
+     *  - Skips generation if keystore is already loaded
+     *
+     * @throws Exception If key generation fails
+     */
     public static void generateKey() throws Exception {
-        KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
-        ks.load(null);
+        KeyStore keyStore = KeyStore.getInstance(KEYSTORE);
+        keyStore.load(null);
 
         // Key already exists, do nothing
-        if (ks.containsAlias(rsaKeyAlias)) {
+        if (keyStore.containsAlias(rsaKeyAlias)) {
             return;
         }
 
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+        createAndStoreKey();
+    }
+
+    /**
+     * Encrypts plaintext data using RSA encryption.
+     *
+     * @param plaintext The data to encrypt
+     * @return Base64-encoded encrypted data
+     * @throws Exception If encryption fails
+     */
+    public String encryptData(String plaintext) throws Exception {
+        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey());
+
+        byte[] encryptedData = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
+        return Base64.encodeToString(encryptedData, Base64.NO_WRAP);
+    }
+
+    /**
+     * Decrypts data (user password) using RSA.
+     *
+     * @param encryptedData The data to decrypt
+     * @return The decrypted plaintext
+     * @throws Exception If decryption fails
+     */
+    public String decryptData(String encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        cipher.init(Cipher.DECRYPT_MODE, getPrivateKey());
+
+        byte[] plaintext = cipher.doFinal(Base64.decode(encryptedData, Base64.NO_WRAP));
+        return new String(plaintext, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Used for signing a string message which consists of a bank command.
+     *
+     * @param message The bank command to be signed
+     * @return The signed bank command
+     * @throws Exception If signing process fails
+     */
+    public static String sign(String message) throws Exception {
+        Signature signature = Signature.getInstance(SIGNING_ALGORITHM);
+        signature.initSign(getPrivateKey());
+        signature.update(message.getBytes(StandardCharsets.UTF_8));
+
+        byte[] signatureBytes = signature.sign();
+        return Base64.encodeToString(signatureBytes, Base64.NO_WRAP);
+    }
+
+    /**
+     * Used for verifying the signature of a bank command.
+     *
+     * @param command The command to be verified
+     * @return Boolean, True if verification is successful
+     * @throws Exception If verification process fails
+     */
+    public static boolean verify(BankCommand command) throws Exception {
+        String payload = command.command + command.amountEuros + command.timestamp
+                + command.nonce;
+        Signature v = Signature.getInstance(SIGNING_ALGORITHM);
+
+        v.initVerify(getPublicKey());
+        v.update(payload.getBytes(StandardCharsets.UTF_8));
+
+        return v.verify(Base64.decode(command.signature, Base64.NO_WRAP));
+    }
+
+    /**
+     * Creates an RSA PKCS1 key for encryption and decryption.
+     * Key is stored in android keystore.
+     *
+     * @throws Exception If key generation fails
+     */
+    private static void createAndStoreKey() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator
+                .getInstance(KeyProperties.KEY_ALGORITHM_RSA, KEYSTORE);
 
         KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
                 rsaKeyAlias,
                 KeyProperties.PURPOSE_SIGN |
                         KeyProperties.PURPOSE_VERIFY |
                         KeyProperties.PURPOSE_ENCRYPT |
-                        KeyProperties.PURPOSE_DECRYPT
-        )
+                        KeyProperties.PURPOSE_DECRYPT)
                 .setDigests(KeyProperties.DIGEST_SHA1)
                 .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
@@ -45,48 +131,29 @@ public class EncryptionHandler {
         keyPairGenerator.generateKeyPair();
     }
 
-
-    public String encryptData(String plaintext) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey());
-        byte[] encryptedData = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
-        return Base64.encodeToString(encryptedData, Base64.NO_WRAP);
-    }
-
-
-    public String decryptData(String encrypted) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, getPrivateKey());
-        byte[] plaintext = cipher.doFinal(Base64.decode(encrypted, Base64.NO_WRAP));
-        return new String(plaintext, "UTF-8");
-    }
-
-    public static String sign(String message) throws Exception {
-        Signature s = Signature.getInstance("SHA1withRSA");
-        s.initSign(getPrivateKey());
-        s.update(message.getBytes("UTF-8"));
-        byte[] sig = s.sign();
-        return Base64.encodeToString(sig, Base64.NO_WRAP);
-    }
-
-    public static boolean verify(BankCommand command) throws Exception {
-        String payload = command.command + command.amountEuros + command.timestamp
-                + command.nonce;
-        Signature v = Signature.getInstance("SHA1withRSA");
-        v.initVerify(getPublicKey());
-        v.update(payload.getBytes("UTF-8"));
-        return v.verify(Base64.decode(command.signature, Base64.NO_WRAP));
-    }
-
+    /**
+     * Helper method that retrieves the public key from AndroidKeystore.
+     *
+     * @return The public key
+     * @throws Exception If retrieval of key fails
+     */
     private static PublicKey getPublicKey() throws Exception {
-        KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+        KeyStore ks = KeyStore.getInstance(KEYSTORE);
         ks.load(null);
+
         return ks.getCertificate(rsaKeyAlias).getPublicKey();
     }
+
+    /**
+     * Helper method that retrieves the private key from AndroidKeystore.
+     *
+     * @return The private key
+     * @throws Exception If retrieval of key fails
+     */
     private static PrivateKey getPrivateKey() throws Exception {
-        KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+        KeyStore ks = KeyStore.getInstance(KEYSTORE);
         ks.load(null);
+
         return (PrivateKey) ks.getKey(rsaKeyAlias, null);
     }
-
 }

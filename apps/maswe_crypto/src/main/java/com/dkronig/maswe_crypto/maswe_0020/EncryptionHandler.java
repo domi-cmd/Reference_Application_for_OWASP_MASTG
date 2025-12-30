a56531cf92e456fac21b608cdf93f6820fea8231
domi-cmd <dokro@gmx.ch>
2025-12-30 15:58:28 +0100

    Cleaned EncryptionHandler for maswe_0020, added docstrings, removed old comments

--- apps/maswe_crypto/src/main/java/com/dkronig/maswe_crypto/maswe_0020/EncryptionHandler.java
+++ apps/maswe_crypto/src/main/java/com/dkronig/maswe_crypto/maswe_0020/EncryptionHandler.java
@@ -2,15 +2,29 @@ package com.dkronig.maswe_crypto.maswe_0020;
 
 import android.util.Base64;
 
+/**
+ * Encryption Handler for MASWE-0020
+ */
 public class EncryptionHandler {
-
-    // Method for encrypting a string by using Base64 encoding
+    /**
+     * Method for encrypting a string (user password) by using Base64 encoding.
+     *
+     * @param plaintext The plaintext to be encoded
+     * @return The base64-encoded plaintext
+     * @throws Exception If encoding fails
+     */
     public String encryptData(String plaintext) throws Exception {
         return Base64.encodeToString(plaintext.getBytes(), Base64.DEFAULT);
     }
 
-    // Method for decrypting a string by using Base64 decoding
-    public String decryptData(String encrypted) throws Exception {
-        return new String(Base64.decode(encrypted, Base64.DEFAULT));
+    /**
+     * Method for decrypting a string (user password) by using Base64 decoding
+     *
+     * @param encryptedData The string to be decrypted
+     * @return The decrypted data
+     * @throws Exception If decryption fails
+     */
+    public String decryptData(String encryptedData) throws Exception {
+        return new String(Base64.decode(encryptedData, Base64.DEFAULT));
     }
 }
